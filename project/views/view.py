820c169f14114dc40d613a83de1d30e8dff05b7d
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2026-01-05 18:10:24 -0300

    Teste

--- project/views/view.py
+++ project/views/view.py
@@ -1,5 +1,5 @@
 def nomeSenha(nome, senha):
     if nome == "Jean Lukas" and senha == "1234":
         return f"Olá {nome}"
-    return "Nome ou Senha incorreto!!"
+    return "Nome ou Senha incorretos!!"
 
